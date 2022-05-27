8aed3cadc93fe2507913e590e4f5fb1c09199df2
mahesh945 <mp.mahesh2200@gmail.com>
2022-05-27 19:18:34 +0530

    new commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
